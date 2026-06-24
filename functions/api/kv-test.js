d92aeb9d2eb137fed136303c722685957fbcea69
uino <127417306+uinosuke@users.noreply.github.com>
2026-06-25 01:56:48 +0900

    Delete functions directory

--- functions/api/kv-test.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env) {
-    await env.WORKS_KV.put("hello", "world");
-    const value = await env.WORKS_KV.get("hello");
-
-    return new Response("KV says: " + value);
-  }
-};
